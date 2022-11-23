cef023cc3ee900bf6a8c9ebda8a074870caf4820
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-11-23 16:15:01 +0530

    gpg changes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import * as core from "@actions/core";
 
 try {
   const result = await main("gpg-signing");
-
-  !result
-    ? core.setOutput("Result", "Success")
-    : core.setFailed("Installation failed");
+  const message = JSON.parse(result);
+  if (message) {
+    core.setOutput("extractPath", message.imp_file_paths.extractPath);
+    core.setOutput("directoryPath", message.imp_file_paths.directoryPath);
+  } else {
+    core.setFailed("Installation Failed");
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
