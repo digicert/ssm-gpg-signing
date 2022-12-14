900ba4d05be08f650d4a20b14aea7a97a4d06067
Yashdigi <108860263+Yashdigi@users.noreply.github.com>
2022-12-14 11:48:18 +0530

    new

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ import os from "os";
 
 const osPlat: string = os.platform();
 const signtools =
-  osPlat == "win32" ? ["smctl", "ssm-scd", "signtool"] : ["smctl", "ssm-scd"];
+  osPlat == "win32" ? ["smctl", "ssm-scd", "signtool","gnupg"] : ["smctl", "ssm-scd"];
 const toolInstaller = async (toolName: string, toolPath: string = "") => {
   let cacheDir;
   switch (toolName) {
@@ -30,6 +30,29 @@ const toolInstaller = async (toolName: string, toolPath: string = "") => {
       break;
     case "ssm-scd":
       cacheDir = await tc.cacheDir(toolPath, toolName, "latest");
+      core.addPath(cacheDir);
+      core.debug(`tools cache has been updated with the path: ${cacheDir}`);
+      break;
+
+      case "gnupg":
+        const downloadUrl =
+          `https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.8.tar.bz2`
+          
+      let downloadPath = "";
+
+      try {
+        downloadPath = await tc.downloadTool(downloadUrl);
+      } catch (err: any) {
+        core.debug(err);
+
+        throw new Error(`failed to download Mage v: ${err.message}`);
+      }
+      // Extract tar
+      const extractPath =
+        osPlat == "win32"
+          ? await tc.extractZip(downloadPath)
+          : await tc.extractTar(downloadPath);
+      cacheDir = await tc.cacheDir(extractPath,'gpg', "latest");
       core.addPath(cacheDir);
       core.debug(`tools cache has been updated with the path: ${cacheDir}`);
       break;
